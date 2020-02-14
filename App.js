aa82eeec1e4d00857391bc7bfdab4054b10df2a0
kikiolivia <kikiolivia54@gmail.com>
2020-02-14 10:11:09 +0700

    first project

---
+++ App.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
+import { uuid } from 'uuidv4';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk' },
+    {id: uuid(), text: 'Eggs' },
+    {id: uuid(), text: 'Bread' },
+    {id: uuid(), text: 'Juice' },
+  ]);
+
+  const [editStatus, editStatusChange] = useState(false);
+
+  const [editItemDetail, editItemDetailChange] = useState({
+    id: null,
+    text: null,
+  });
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
+  const saveEditItem = (id, text) => {
+    setItems(prevItems => {
+      return prevItems.map(item => 
+          item.id === editItemDetail.id ? {id, text: editItemDetail.text} : item,
+        );
+    });
+    editStatusChange(!editStatus);
+  }
+
+  const handleEditChange = text => {
+    editItemDetailChange({id: editItemDetail.id, text});
+  }
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert(
+        'No item entered',
+        'Please enter an item when adding to your shopping list',
+        [
+          {
+            text: 'Understood',
+            style: 'cancel'
+          },
+        ],
+        {cancelable: true},
+      );
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  }
+
+  const editItem = (id, text) => {
+    editItemDetailChange({
+      id,
+      text,
+    });
+    return editStatusChange(!editStatus);
+  }
+
+  const itemChecked = (id, text) => {
+    const isChecked = checkedItems.filter(checkedItem => checkedItem.id === id);
+    isChecked.length
+      ? // remove item from checked items state (uncheck)
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id)];
+        })
+      : // Add item to checked items state
+      checkedItemChange(prevItems => {
+        return [...prevItems.filter(item => item.id !== id), {id, text}];
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem 
+        addItem={addItem} />
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => <ListItem item={item}
+        deleteItem={deleteItem} />} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // paddingTop: 60,
+  },
+});
+
+export default App;
